af25ef611c02e31d974ff21044161051c157806d
casperlee <casperlee@thinkgeo.com>
2018-05-17 15:43:47 +0800

    Add RoutingSample for Android.

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # RoutingSample-ForAndroid
-The Map Suite Routing “How Do I?” solution offers a series of useful how-to examples for using the Map Suite Routing extension. The bundled solution comes with a small set of routable street data from Austin, TX and demonstrates simple routing, avoiding specific areas, getting turn-by-turn directions, optimizing for the Traveling Salesman Problem, and much more. Full source code is included in both C# and VB.NET languages; simply select your preferred language to download the associated solution.
+
+### Description
+
+
+The Map Suite Routing “How Do I?” solution offers a series of useful how-to examples for using the Map Suite Routing extension. The bundled solution comes with a small set of routable street data from Newfoundland Island Realty, Canada and demonstrates simple routing, avoiding specific areas, getting turn-by-turn directions, optimizing for the Traveling Salesman Problem, and much more. Full source code is included in both C# and VB.NET languages; simply select your preferred language to download the associated solution.
+
+Please refer to [Wiki](http://wiki.thinkgeo.com/wiki/map_suite_mobile_for_android) for the details.
+
+![Screenshot](https://github.com/ThinkGeo/RoutingSample-ForAndroid/blob/master/ScreenShot.png)
+
+### Requirements
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0](https://www.nuget.org/packages?q=ThinkGeo)
+
+### About the Code
+```csharp
+var routingSource = new RtgRoutingSource(DataManager.GetDataPath("land_lnd_street_segment_route.rtg"));
+var featureSource = new ShapeFileFeatureSource(DataManager.GetDataPath("land_lnd_street_segment_route.routable.shp"));
+featureSource.Open();
+var boundry = featureSource.GetBoundingBox();
+routingEngine = new RoutingEngine(routingSource, featureSource);
+routingEngine.GeographyUnit = GeographyUnit.Meter;
+routingEngine.SearchRadiusInMeters = 2000000;
+```
+### Getting Help
+
+[Map Suite Web for Android Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_mobile_for_android)
+
+[Map Suite Web for Android Product Description](https://thinkgeo.com/mobile)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+This example makes use of the following APIs:
+
+- [ThinkGeo.MapSuite.Routing.RtgRoutingSource](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.routing.rtgroutingsource)
+- [ThinkGeo.MapSuite.Layers.ShapeFileFeatureSource](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.layers.shapefilefeaturesource)
+- [ThinkGeo.MapSuite.Routing.RoutingEngine](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.routing.routingengine)
+- [ThinkGeo.MapSuite.Shapes.RectangleShape](http://wiki.thinkgeo.com/wiki/api/thinkgeo.mapsuite.shapes.rectangleshape)
+
+### About Map Suite
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
---
+++ RoutingSampleAndroid/MainActivity.cs
@@ -0,0 +1,100 @@
+﻿using Android.App;
+using Android.Graphics;
+using Android.OS;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using ThinkGeo.MapSuite;
+using ThinkGeo.MapSuite.Android;
+using ThinkGeo.MapSuite.Layers;
+using ThinkGeo.MapSuite.Routing;
+using ThinkGeo.MapSuite.Shapes;
+using ThinkGeo.MapSuite.Styles;
+
+namespace RoutingSample
+{
+    [Activity(Label = "RoutingSample", Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private MapView mapView;
+        private LayerOverlay layerOverlay;
+        private RoutingLayer routingLayer;
+        private RoutingEngine routingEngine;
+        private bool firstClick = true;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+            try
+            {
+                FrameLayout mapContainerView = FindViewById<FrameLayout>(Resource.Id.MapContainerView);
+                mapContainerView.RemoveAllViews();
+                mapView = new MapView(Application.Context);
+                mapView.SetBackgroundColor(Color.Argb(255, 244, 242, 238));
+
+                InitalizeMap();
+
+                mapContainerView.AddView(mapView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FillParent, ViewGroup.LayoutParams.FillParent));
+            }
+            catch (Exception ex)
+            {
+                Log.Debug("Sample Changed", ex.Message);
+            }
+        }
+
+        private void InitalizeMap()
+        {
+            mapView.MapUnit = GeographyUnit.Meter;
+            mapView.SingleTap += MapView_SingleTap;
+
+            LayerOverlay backgroundOverlay = new LayerOverlay();
+            ShapeFileFeatureLayer shapeFileFeatureLayer = new ShapeFileFeatureLayer(DataManager.GetDataPath("land_lnd_street_segment_route.routable.shp"));
+            shapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.DefaultLineStyle = WorldStreetsLineStyles.MotorwayFill(2.0f);
+            shapeFileFeatureLayer.ZoomLevelSet.ZoomLevel01.ApplyUntilZoomLevel = ApplyUntilZoomLevel.Level20;
+            backgroundOverlay.Layers.Add(shapeFileFeatureLayer);
+            mapView.Overlays.Add(backgroundOverlay);
+
+            layerOverlay = new LayerOverlay();
+            routingLayer = new RoutingLayer();
+            layerOverlay.Layers.Add(routingLayer);
+            mapView.Overlays.Add(layerOverlay);
+
+            var routingSource = new RtgRoutingSource(DataManager.GetDataPath("land_lnd_street_segment_route.rtg"));
+            var featureSource = new ShapeFileFeatureSource(DataManager.GetDataPath("land_lnd_street_segment_route.routable.shp"));
+            featureSource.Open();
+            var boundry = featureSource.GetBoundingBox();
+            routingEngine = new RoutingEngine(routingSource, featureSource);
+            routingEngine.GeographyUnit = GeographyUnit.Meter;
+            routingEngine.SearchRadiusInMeters = 2000000;
+
+            mapView.CurrentExtent = new RectangleShape(307619.124092, 900219997.607509, 121246866.13256, 800097784.115004);
+        }
+
+        private void MapView_SingleTap(object sender, SingleTapMapViewEventArgs e)
+        {
+            routingLayer.Routes.Clear();
+            if (firstClick)
+            {
+                routingLayer.StartPoint = e.WorldPoint;
+                firstClick = false;
+            }
+            else
+            {
+                routingLayer.EndPoint = e.WorldPoint;
+                firstClick = true;
+            }
+
+            routingLayer.Routes.Clear();
+            if (routingLayer.StartPoint != null && routingLayer.EndPoint != null)
+            {
+                RoutingResult routingResult = routingEngine.GetRoute(routingLayer.StartPoint, routingLayer.EndPoint);
+                routingLayer.Routes.Add(routingResult.Route);
+            }
+
+            layerOverlay.Refresh();
+        }
+    }
+}
+
